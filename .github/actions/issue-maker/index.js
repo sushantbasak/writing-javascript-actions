0aa893d4719dcd26cb35c3f21fd35b5b75b4a235
sushantbasak <sushantbasak73@gmail.com>
2021-06-17 12:33:03 +0530

    Patch console only in try block in issue-maker

--- .github/actions/issue-maker/index.js
+++ .github/actions/issue-maker/index.js
@@ -1,20 +1,21 @@
-const core = require("@actions/core");
-const github = require("@actions/github");
+const core = require('@actions/core');
+const github = require('@actions/github');
 
 async function run() {
   try {
-    const issueTitle = core.getInput("issue-title");
-    const jokeBody = core.getInput("joke");
-    const token = core.getInput("repo-token");
+    // const issueTitle = core.getInput("issue-title");
+    // const jokeBody = core.getInput("joke");
+    // const token = core.getInput("repo-token");
 
-    const octokit = new github.getOctokit(token);
+    // const octokit = new github.getOctokit(token);
 
-    const newIssue = await octokit.issues.create({
-      repo: github.context.repo.repo,
-      owner: github.context.repo.owner,
-      title: issueTitle,
-      body: jokeBody
-    });
+    // const newIssue = await octokit.issues.create({
+    //   repo: github.context.repo.repo,
+    //   owner: github.context.repo.owner,
+    //   title: issueTitle,
+    //   body: jokeBody
+    // });
+    console.log('Hello');
   } catch (error) {
     core.setFailed(error.message);
   }
